057188eb46a3dbd43685781a2730260c884a21de
embresse <115603992+embresse@users.noreply.github.com>
2023-04-14 17:04:12 -0400

    updated src-sw file

--- client/src-sw.js
+++ client/src-sw.js
@@ -27,7 +27,6 @@ warmStrategyCache({
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
-const { StaleWhileRevalidate } = require('workbox-strategies');
 
 const assetsCache = new StaleWhileRevalidate({
   cacheName: 'assets-cache',
