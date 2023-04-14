81267ece3f83289a781ebb70b4941f75d14d7121
embresse <115603992+embresse@users.noreply.github.com>
2023-04-14 14:26:52 -0400

    initial commit

--- Develop/client/src-sw.js
+++ Develop/client/src-sw.js
@@ -27,4 +27,19 @@ warmStrategyCache({
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
-registerRoute();
+const { StaleWhileRevalidate } = require('workbox-strategies');
+
+const assetsCache = new StaleWhileRevalidate({
+  cacheName: 'assets-cache',
+  plugins: [
+    new CacheableResponsePlugin({
+      statuses: [0, 200],
+    }),
+    new ExpirationPlugin({
+      maxAgeSeconds: 7 * 24 * 60 * 60, // Cache for 7 days
+    }),
+  ],
+});
+
+
+registerRoute(assetsCache);
--- Develop/client/src/js/install.js
+++ Develop/client/src/js/install.js
@@ -2,10 +2,37 @@ const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+window.addEventListener('beforeinstallprompt', (event) => {
+    window.deferredPrompt = event;
+    // sets property to event object
+    // deferredPrompt assigned to window object as global property
+    butInstall.classList.toggle('hidden', false);
+    // removes hidden class from buttonInstall -> visible to user
+});
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener('click', async () => {
+    const prompt = window.deferredPrompt;
+
+    if (!prompt){
+        return;
+    }
+    // checks if falsy, if falsy returns and nothing happens
+
+    prompt.prompt();
+    // shows prompt 
+    // if truthy, prompt() method called
+
+    window.deferredPrompt = null;
+    // reset deferredPrompt
+    // keeps prompt from installing multiple times
+
+    butInstall.classList.toggle('hidden', true);
+    // hides install button if install initiated after prompt() called
+});
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener('appinstalled', (event) => {
+    window.deferredPrompt = null;
+    // keeps prompt from installing multiple times
+});
